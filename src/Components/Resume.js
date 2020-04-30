33c888c11225acec7f16d18030f348c4c6877616
gabeaster <gab.easter@live.com>
2020-04-29 21:27:22 -0400

    added resume file

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,23 +1,15 @@
 import React from "react";
-
+import { Document, Page } from "react-pdf";
+import resumePDF from "../assets/resume.pdf";
 function Resume() {
   return (
     <div className="components resume">
       <div className="twinkling"></div>
       <div className="clouds"></div>
       <div className="resume-container">
-        <h1>Content Title</h1>
-        <p>
-          Voluptate esse enim commodo magna ea. Reprehenderit sint magna dolore
-          velit ea id. Sint sit officia anim magna cillum et qui elit.
-        </p>
-        <p>
-          Eiusmod ad commodo occaecat ullamco adipisicing eu in. Adipisicing ut
-          deserunt eiusmod dolore sit eiusmod dolor quis sint irure nulla sunt
-          voluptate. Cupidatat nulla culpa irure nisi incididunt id. Amet
-          consectetur aliqua nisi nulla officia in. Commodo sunt velit fugiat
-          pariatur eiusmod.
-        </p>
+        <Document file={resumePDF} onLoadSuccess={this.onDocumentLoadSuccess}>
+          <Page pageNumber={1} />
+        </Document>
       </div>
     </div>
   );
