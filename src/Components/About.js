9e426e80cc91a0a50b1975e93b2123decb7aa20a
gabeaster <gab.easter@live.com>
2020-05-07 02:02:25 -0400

    cleaned up code, added some styles

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 
 function About() {
-  //pseudo
-  //if hover is true for .developer-container, #dev-flip adds class "show"; if .developer-container is false, #dev-flip should not have class "show"
   const [showDev, setShowDev] = useState(false);
   const [showLead, setShowLead] = useState(false);
   const [showEd, setShowEd] = useState(false);
@@ -13,12 +11,7 @@ function About() {
       <div className="twinkling"></div>
       <div className="clouds"></div>
       {showHov && (
-        <p
-          className="animated fast heartBeat hover-img "
-          // src={require("../assets/hover me.png")}
-        >
-          Hover Me
-        </p>
+        <p className="animated fast heartBeat hover-img ">Hover Me</p>
       )}
 
       <div className="about-containers" id="top-container">
@@ -84,24 +77,46 @@ function About() {
       <div className="paragraph-container">
         {showDev && (
           <div id="dev" className="developer-flip flip-item item">
-            <p>I am a Front-End Developer with a hawk-eye for detail. </p>
+            <p>
+              As a developer with a hawk-eye for detail, I've logged over a
+              thousand hours of coding and have been endorsed for more than 20
+              skills including Front-End Development, React JS, and Responsive
+              Design on LinkedIn. Pair-programming, brainstorming, and
+              collaborating with peers ensures I'm constantly learning and
+              pushing my skills further. What I love most about being a
+              developer is diving into projects knowing that I have the
+              resources to solve problems and build beautiful designs.
+            </p>
           </div>
         )}
-
         {showLead && (
           <div id="lead" className="leader-flip flip-item item">
             <p>
-              I believe that the success of a team depends on the leader. I've
-              won Homer Awards for bolstering my team members and having my work
-              implemented as best practices.
+              Understanding how to organize, prioritize, and delegate tasks
+              while working on a team is vital to the success and quality of the
+              final product. I have experience setting clear, easy-to-understand
+              agile tasks while tracking and managing project progression on
+              Trello.
+              <br />
+              With cross-cultural and cross-departmental leadership experience,
+              I know that the communication and culture of a team can fuel
+              excitement, passion, and a drive to deliver.
             </p>
           </div>
         )}
         {showEd && (
           <div id="ed" className="educator-flip flip-item item">
             <p>
-              With more than 5 years of teaching experience, I've learned that
-              compassion and empathy towards students is vital.
+              As an educator and curriculum developer, it's been my job to
+              tailor the learning experience to the needs of the group and the
+              individual. Time-management is a Director of Education's best
+              friend while planning, training, evaluating, and teaching a full
+              class load. Whether I'm teaching children their colors in English
+              or training new team members on best practices, I never forget my
+              4 C's:
+              <p className="center">
+                Compassion, Creativity, Collaboration, and Coaching.
+              </p>
             </p>
           </div>
         )}
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -3,8 +3,6 @@ import Capture from "./slideComponents/capture";
 import Todo from "./slideComponents/todo";
 import Tracker from "./slideComponents/cryptoTracker";
 function Slider() {
-  //add components to the array
-  //array for component to show inside the slider
   let sliderArr = [<Capture />, <Todo />, <Tracker />];
   const [x, setX] = useState(0);
   console.log(x);
@@ -30,13 +28,9 @@ function Slider() {
       })}
 
       <button id="back-btn" className="slide-btn" onClick={goBack}>
-        {/* <div className="selected-link A"></div>
-        <div className="selected-link D"></div> */}
         <h3>Back</h3>
       </button>
       <button id="next-btn" className="slide-btn" onClick={goNext}>
-        {/* <div className="selected-link B"></div>
-        <div className="selected-link C"></div> */}
         <h3>Next</h3>
       </button>
     </div>
